a00805cc48661a43c7a8d13bea8f9284df098772
Kamaco93 <Salande93>
2023-10-31 13:50:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposecatalog/ConstraintLayoutExample.kt
@@ -0,0 +1,79 @@
+package com.example.jetpackcomposecatalog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+
+@Composable
+fun ConstrainLayoutExample() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        val (boxRed, boxBlue, boxGreen, boxMagenta, boxCyan) = createRefs()
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Cyan)
+            .constrainAs(boxCyan) {
+                top.linkTo(boxRed.bottom)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ConstrainLayoutExampleTwo() {
+    ConstraintLayout {
+        val boxRed = createRef()
+        val topGuide = createGuidelineFromTop(0.1f)
+        val starGuide = createGuidelineFromStart(0.50f)
+
+        Box(modifier = Modifier
+            .size(126.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(topGuide)
+                start.linkTo(starGuide)
+            })
+
+    }
+
+
+}
